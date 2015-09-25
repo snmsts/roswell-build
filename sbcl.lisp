be96ef943f43159d9adb0b92d310a73e94e0c119
SANO Masatoshi <snmsts@gmail.com>
2015-09-26 00:59:16 +0900

    try release..

--- sbcl.lisp
+++ sbcl.lisp
@@ -2,7 +2,7 @@
 (mapc (lambda (x)(load (format nil "~A/~A~A" (uiop:getenv "ROSWELL_INSTALL_DIR") "share/common-lisp/source/roswell/"x)))
       '("install.lisp" "install-sbcl.lisp"))
 
-(defparameter *version* (first (split-sequence:split-sequence #\newline (ros:roswell '("list" "versions" "sbcl")))))
+(defparameter *version* (or *version* (first (split-sequence:split-sequence #\newline (ros:roswell '("list" "versions" "sbcl"))))))
 
 (defun extract-sbcl ()
   (let ((version *version*))
@@ -29,8 +29,40 @@
                       :output out)))
 
 (defun archive-sbcl (arch)
-  ;;not yet
   (let* ((version *version*)
          (archive-name (format nil "sbcl-~A-~A-~A" version arch (uname)))
-         (archive-dir (merge-pathnames (format nil "tmp/~A/" archive-name) (homedir))))
-    (ensure-directories-exist archive-dir)))
+         (archive-dir (merge-pathnames (format nil "tmp/~A/" archive-name) (user-homedir-pathname))))
+    (ensure-directories-exist archive-dir)
+    (let ((from (truename (format nil "~~/src/sbcl-~A/" version)))
+          (to archive-dir))
+      (flet ((copy (from to)
+               (ensure-directories-exist to)
+               (uiop:copy-file from to))
+             (touch (file)
+               (ensure-directories-exist file)
+               (with-open-file (i file
+                                  :direction :probe
+                                  :if-does-not-exist :create))))
+        (loop :for (method . elts) :in ros.install::*sbcl-copy-files*
+           :do (case method
+                 (:copy (loop for elt in elts
+                           do (if (and (stringp elt) (wild-pathname-p elt))
+                                  (mapc (lambda (x)
+                                          (copy x (make-pathname :defaults x
+                                                                 :directory (append (pathname-directory to)
+                                                                                    (nthcdr (length (pathname-directory from))
+                                                                                            (pathname-directory x))))))
+                                        (reverse (directory (merge-pathnames elt from))))
+                                  (if (consp elt)
+                                      (progn
+                                        (copy (merge-pathnames (first elt) from)
+                                              (merge-pathnames (first elt) to))
+                                        (sb-posix:chmod (merge-pathnames (first elt) to) (second elt)))
+                                      (copy (merge-pathnames elt from)
+                                            (merge-pathnames elt to))))))
+                 (:touch (loop for elt in elts
+                            do (if (functionp elt)
+                                   (funcall elt from to #'touch))))))
+        (uiop:chdir (merge-pathnames "../" archive-dir))
+        (uiop:run-program `("tar""cjf" ,(format nil "~A.tar.bz2" archive-name) ,archive-name) :output :interactive)
+        (truename (merge-pathnames (format nil "../~A.tar.bz2" archive-name) archive-dir))))))
